aa2f2667c1722c628cf1a7286cf7d359c1e2be75
RupanjanBhattacharyya <quest4knowledge1035@gmail.com>
2024-03-03 15:16:44 +0530

    ChatBot Projet

---
+++ android/app/src/main/kotlin/com/example/chatbotproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatbotproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/api_key.dart
@@ -0,0 +1,3 @@
+class GeminiApiKey {
+  static const api_key = "AIzaSyCTQzBa7UQ9wwn7utTsG027CVYRNWX7fWQ";
+}
